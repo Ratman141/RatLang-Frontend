1701ae2696875910f42252cd38fff7673cc5d670
Ratman141 <149208690+Ratman141@users.noreply.github.com>
2026-03-19 22:43:45 +0100

    Made invalid command error more intuitive

--- src/commands/commands.cpp
+++ src/commands/commands.cpp
@@ -44,7 +44,9 @@ namespace cmd{
             run(runFile);
         }
         else{
-            std::cerr<<"Error: Invalid command. Use 'help' for a list of commands.\n";
+            std::cerr<<"Error: Invalid command. Use '--help' for a list of commands.\n";
+            std::cerr<<"Usage: ratlang <command> [arguments]\n";
+            std::cerr<<"If you want to run a file, use 'ratlang --run <file.rl>'\n";
             throw std::runtime_error("Invalid command");
         }
     }
